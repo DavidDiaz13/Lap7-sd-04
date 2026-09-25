b0ba6322d62c2c72d84f45f9886ef9c02bcf015f
“DavidDiaz” <diazdadp@gmail.com>
2026-09-25 13:27:47 -0600

    lap7

---
+++ task6.js
@@ -0,0 +1,9 @@
+const arr1 = ["hello"]
+const arr2 = ["world"]
+
+// Type your code below this line!
+
+const nuevoArray = arr1.concat(arr2);
+console.log(nuevoArray);
+
+// Type your code above this line!
